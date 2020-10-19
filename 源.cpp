fcae02defcf8c0ec992b79b7e0e817f8daa873fe
AnmoWu <73103395+AnmoWu@users.noreply.github.com>
2020-10-19 23:30:50 +0800

    First
    A nice try

---
+++ 源.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello, World!";
+	return 0;
+}
+git remote add origin https ://github.com/AnmoWu/Wynn.git
+git branch - M main
+git push - u origin main
